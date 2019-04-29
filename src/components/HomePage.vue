882f36855b5f1bf8a9747deb5b9ebaab2620bc9e
Jacob Adams <Artchecks@gmail.com>
2019-04-28 17:44:39 -0700

    begin work on displaying products

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,10 +1,68 @@
 <template>
-  
+  <div>
+    <v-container grid-list-md>
+      <v-layout>
+        <v-flex md3 sm6 xs12 v-for="(column, i) in gridColumns" :key="i">
+          <v-layout column>
+            <v-flex v-for="(element, index) in column" :key="index">
+              <product-list
+                :product="element"
+              />
+            </v-flex>
+          </v-layout>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 <script>
+import ProductsList from './ProductsList.vue'
 export default {
-  name: 'HomePage'
+  name: 'HomePage',
+  computed: {
+    dealsOfDay () {
+      return this.$store.getters.dealsOfDay
+    },
+    selectedProducts () {
+      return this.$store.getters.selectedProducts
+    },
+    gridColumns () {
+      let i = 0
+      let j = 0
+      let columns
+      if (this.$vuetify.breakpoint.xs) {
+        columns = []
+        columns.push(this.getCheatSheet)
+        return columns       
+      } else if (this.$vuetify.breakpoint.sm) {
+        columns = [[], []]
+        while (i < this.getCheatSheet.length) {
+          columns[j].push(this.getCheatSheet[i])
+          i += 1
+          j += 1
+          if (j === columns.length) {
+            j = 0
+          }
+        }
+        return columns 
+      } else if (this.$vuetify.breakpoint.mdAndUp) {
+        columns = [[], [], [], []]
+        while (i < this.getCheatSheet.length) {
+          columns[j].push(this.getCheatSheet[i])
+          i += 1
+          j += 1
+          if (j === columns.length) {
+            j = 0
+          }
+        }
+        return columns
+      }
+    }
+  },
+  compoents: {
+    ProductsList
+  }
 }
 </script>
 
---
+++ src/components/ProductsList.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <v-card>
+      <v-card-title class="white--text">
+        {{ product.category }}
+      </v-card-title>
+      <v-divider dark/>
+      <v-layout row @click="show = !show">
+        <v-flex xs10>
+          <v-card-title class="white--text font-weight-bold">{{ product.example }}</v-card-title>
+        </v-flex>
+        <v-flex xs2 class="mr-3">
+          <v-btn icon >
+            <v-icon class="white--text">{{ show ? 'keyboard_arrow_up' : 'keyboard_arrow_down'}}</v-icon>
+          </v-btn>
+        </v-flex>
+      </v-layout>
+      <v-slide-y-transition>
+        <v-card-text v-show="show"
+          class="white--text">{{ product.purpose }}</v-card-text>
+      </v-slide-y-transition>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductsList',
+  data () {
+    return {
+      show: false
+    }
+  },
+  props: {
+    product: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+  
+</style>
